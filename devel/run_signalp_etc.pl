90c898f43641f38cc359ac51e7c5756babea2682
Seth Schobel <sschobel@sschobelvm-lx.jcvi.org>
2015-03-27 13:00:32 -0400

    added prototype signalp service to repsoitory

---
+++ devel/run_signalp_etc.pl
@@ -0,0 +1,52 @@
+#!/usr/local/bin/perl
+
+use strict;
+use Config::IniFiles;
+use FindBin;
+require "$FindBin::Bin/pipeline_lib.pl";
+
+&print_time("STARTTIME");
+
+my $file = $ARGV[0];
+my $results_path = $ARGV[1];
+my $config = $ARGV[2];
+
+my $program_path = $0;
+my @prog = split '/', $program_path;
+my $program = pop @prog;
+my $input_type = "LipoproteinMotifBSML";
+my @files;
+
+my $cfg;
+if ($config) {
+	$cfg = Config::IniFiles->new( -file => "$config" ) || die "cannot parse user suplied config file.\n";
+}
+my $path = &get_lib_path($cfg);
+
+my $id_rep = "$results_path/id_repository";
+system("touch $id_rep");
+
+#use lib "/usr/local/devel/VIRIFX/software/VGD/lib";
+#use Getopt::Euclid 0.2.4 qw(:vars);
+
+my $signalp = "/usr/local/bin/signalp";
+#my $parser = "$FindBin::Bin/camera_parse_annotation_results_to_text_table.pl";
+my $shell_config = "$FindBin::Bin/etc/shell.config";
+my $shell_template = &write_shell_template($shell_config,$path,$results_path);
+
+my $in_name = &get_file_name($file);
+$in_name =~ s/.fasta//;
+my $outfile = "$results_path/signalp_results.$in_name.gff";
+
+my $signalp_cmd = "$signalp -t euk -n $outfile -v $file ";
+print "$signalp_cmd\n";
+my $program = &get_file_name($signalp);
+&run_shell_script($signalp_cmd,$shell_template,$program,$results_path);
+
+#my $parsed_file = &run_parser_script($shell_template,$results_path,$outfile,$parser,$results_path,$input_type);
+
+#my $sed_cmd = "sed 's/__\t/_+\t/' $parsed_file -i";
+#print "$sed_cmd\n";
+#system($sed_cmd);
+
+&print_time("ENDTIME");
---
+++ devel/signalp.pl
@@ -0,0 +1,147 @@
+#!/usr/local/bin/perl
+
+=head1 NAME
+    
+    .pl
+    
+=head1 USAGE
+
+    .pl [-]-results_path <results_path> [-]-infile <infile> [-]-project_code <project_code>
+
+=head1 REQUIRED ARGUMENTS
+
+=over
+
+=item [-]-infile  <infile>
+
+file containing a full path list of all multifasta files to process through the pipeline.
+
+=for Euclid:
+    infile.type: readable
+
+=item [-]-results_path  <results_path>
+
+full path to the location results files should be written
+
+=for Euclid:
+    results_path.type: string
+
+=item [-]-project_code  <project_code>
+
+charge code for using the grid
+
+=for Euclid:
+    project_code.type: string
+
+=item [-]-service  <service>
+
+service type to indicate where to look for optional parameter configurations
+
+=for Euclid:
+    service.type: string
+    
+=back
+
+=head1 OPTIONS
+
+=over
+
+=item [-]-queue  <queue>
+
+Specifies a queue for the grid jobs
+   
+=for Euclid:
+    queue.type: string
+
+=item [-]-config  <config>
+
+Specifies a config file (ini format) used to overide standard pipeline configurations and default parameters
+
+=for Euclid:
+	config.type: string
+	
+=back
+
+=head1 DESCRIPTION
+
+This script will run .
+
+=cut
+
+use strict;
+use FindBin;
+require "$FindBin::Bin/pipeline_lib.pl";
+
+use lib "/usr/local/devel/VIRIFX/software/VGD/lib";
+use Getopt::Euclid 0.2.4 qw(:vars);
+
+my $list = $ARGV_infile;
+my $results_path = $ARGV_results_path;
+my $queue = $ARGV_queue;
+my $config = $ARGV_config;
+my $grid_code = $ARGV_project_code;
+my $service = $ARGV_service;
+
+my $program_path = $0;
+my @prog = split '/', $program_path;
+my $program = pop @prog;
+#my $input_type = "LipoproteinMotifBSML";
+my @files;
+
+my $id_rep = "$results_path/id_repository";
+system("touch $id_rep");
+
+my $fasta_split = "$FindBin::Bin/fasta_splitter.pl";
+my $run_lipo = "$FindBin::Bin/run_signalp_etc.pl";
+
+@files = &read_list_file($list);
+
+my $fasta_file =  "$results_path/all.fasta";
+system "cat @files > $fasta_file";
+
+my $cmd = "$fasta_split $fasta_file $results_path 10000 no";
+print "$cmd\n";
+system $cmd;
+my $list2 = "$results_path/partitions.list";
+
+my @files2 = &read_list_file($list2);
+
+my @results_files;
+foreach my $file (@files2) {
+	my @parts = split '/', $file;
+	my $in_name = pop @parts;
+	$in_name =~ s/.fasta//;
+	my $dir = "$results_path/partitions/$in_name";
+	my $outfile = "$dir/signalp_results.$in_name.gff";
+	
+	my $parsed_file = $outfile . ".parsed";	
+	
+	push @results_files, $parsed_file;
+}
+
+my @JOBS;
+
+my $dir = "$results_path/partitions/fasta" . '$SGE_TASK_ID';
+my $file = "$dir/fasta" . '$SGE_TASK_ID.fasta';
+
+my $run_cmd = "$run_lipo $file $dir";
+if ($config) {
+	$run_cmd .= " $config";
+}
+print "$run_cmd\n";
+
+my $sh_script = write_shell_script($results_path,"${program}",$run_cmd);
+
+my $max_job_array = @files2;
+
+my $job_id = launch_grid_job( $sh_script, $queue, $max_job_array, $results_path, $grid_code);
+push @JOBS, $job_id;
+
+print "waiting for jobs...\n";
+wait_for_grid_jobs_arrays( \@JOBS,1,$max_job_array ) if ( scalar @JOBS );
+
+print "All jobs complete.\n";
+
+my $combined = "$results_path/signalp_combined_results.parsed";
+my $cat = "cat @results_files > $combined";
+system "$cat";
